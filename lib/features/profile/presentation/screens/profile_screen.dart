41ffefc6350a9b8023dc6e9dfc059df8555bb157
EmirC <emirchim369@gmail.com>
2026-03-07 18:45:20 -0600

    perfile

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:satoshimex/core/config/constants/app_colors.dart';
+import 'package:satoshimex/features/profile/presentation/widgets/profile_widgets.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text("Profile", style: TextStyle(color: Colors.white)),
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          children: const [
+            ProfileHeader(),
+            SizedBox(height: 32),
+            AchievementSection(),
+            SizedBox(height: 32),
+            SettingsSection(),
+            SizedBox(height: 24),
+            LogoutTile(),
+            SizedBox(height: 40),
+          ],
         ),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: const Text("Perfil", style: TextStyle(color: Colors.white)),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.settings, color: Colors.white),
+          onPressed: () => print("Abrir ajustes generales"),
+        ),
+      ],
     );
   }
 }
